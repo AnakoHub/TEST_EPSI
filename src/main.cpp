d1619c5488513a1a8c9dd82a6e3d84b99079f708
Anako <anaelakue60@gmail.com>
2020-05-05 11:39:03 +0200

    Premier commit

---
+++ src/main.cpp
@@ -0,0 +1,175 @@
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <PubSubClient.h> //Librairie pour la gestion Mqtt
+#include "main.h"
+
+#define DHTPIN 14     // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+//WIFI
+const char* ssid = "*****************";
+const char* password = "**************";
+//MQTT
+const char* mqtt_server = "test.mosquitto.org";//Adresse IP du Broker Mqtt
+const int mqttPort = 1883; //port utilisé par le Broker
+long tps=0;
+
+#define MQTT_BROKER "local mosquitto"
+#define MQTT_BROKER_PORT  1883
+
+ESP8266WiFiMulti WiFiMulti;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  setup_mqtt();
+  client.publish("EPSI/TEST/EDMOND", "Hello from ESP8266");
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHT11 Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void loop() {
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+  reconnect();
+  client.loop();
+  //On utilise pas un delay pour ne pas bloquer la réception de messages
+  if (millis()-tps>2000){
+     tps=millis();
+     float temp = random(30);
+     mqtt_publish("EPSI/TEST/EDMOND", float(event.temperature));
+     Serial.print("qqchose : ");
+     Serial.println(temp);
+   }
+}
+
+void setup_wifi(){
+  //connexion au wifi
+  WiFiMulti.addAP(ssid, password);
+  while ( WiFiMulti.run() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  Serial.println("");
+  Serial.println("WiFi connecté");
+  Serial.print("MAC : ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Adresse IP : ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup_mqtt(){
+  client.setServer(mqtt_server, mqttPort);
+  client.setCallback(callback);//Déclaration de la fonction de souscription
+  reconnect();
+}
+
+//Callback doit être présent pour souscrire a un topic et de prévoir une action
+void callback(char* topic, byte *payload, unsigned int length) {
+   Serial.println("-------Nouveau message du broker mqtt-----");
+   Serial.print("Canal:");
+   Serial.println(topic);
+   Serial.print("donnee:");
+   Serial.write(payload, length);
+   Serial.println();
+   if ((char)payload[0] == '1') {
+     Serial.println("LED ON");
+      digitalWrite(2,HIGH);
+   } else {
+     Serial.println("LED OFF");
+     digitalWrite(2,LOW);
+   }
+ }
+
+ void reconnect(){
+  while (!client.connected()) {
+    Serial.println("Connection au serveur MQTT ...");
+    if (client.connect("LNIFONPAPHIVOPZ")) {
+      Serial.println("MQTT connecté");
+    }
+    else {
+      Serial.print("echec, code erreur= ");
+      Serial.println(client.state());
+      Serial.println("nouvel essai dans 2s");
+    delay(2000);
+    }
+  }
+  client.subscribe("EPSI/TEST/EDMOND");//souscription au topic led pour commander une led
+}
+
+//Fonction pour publier un float sur un topic
+void mqtt_publish(String topic, float t){
+  client.publish(topic.c_str(), t);
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+
+void reconnect();
+void callback(char* , byte *, unsigned int );
+void setup_wifi();
+void setup_mqtt();
+void mqtt_publish(String , float );
+void loop();
+void setup();
